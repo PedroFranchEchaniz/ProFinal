62f120cf683e76354f5b7da738235ce43416ad3b
Pedro Franch <pedrofe.av@gmail.com>
2023-04-19 14:14:45 +0200

    first commit

---
+++ src/main/java/com/salesianos/triana/dam/e01holamundo/PrincipioProyFinalApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.dam.e01holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipioProyFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipioProyFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianos/triana/dam/e01holamundo/PrincipioProyFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.dam.e01holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipioProyFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
