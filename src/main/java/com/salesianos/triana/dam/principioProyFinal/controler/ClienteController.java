0ab0c57fcccfc8208ab0539dc93f17c27979c04e
Pedro Franch <pedrofe.av@gmail.com>
2023-05-08 10:13:46 +0200

    plantilla actualizacion de datos y recuperacion de usuario a través del id

--- src/main/java/com/salesianos/triana/dam/principioProyFinal/controler/ClienteController.java
+++ src/main/java/com/salesianos/triana/dam/principioProyFinal/controler/ClienteController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianos.triana.dam.principioProyFinal.model.Cliente;
 import com.salesianos.triana.dam.principioProyFinal.service.ClienteServicio;
+
 
 @Controller
 public class ClienteController {
@@ -20,12 +22,19 @@ public class ClienteController {
 	@GetMapping("/nuevoCliente")
 	public String formularioCliente(Model model) {
 		model.addAttribute("cliente", new Cliente());
-		return "formularioAltaCliente";
+		return "login";
 	}
 	
 	@PostMapping("/nuevo/submit")
 	public String procesarFormularioCliente(@ModelAttribute("cliente") Cliente c) {
 		clienteServicio.save(c);
-		return "redirect:/nuevoCliente";
+		return "portada2";
 	}
+	
+	@GetMapping("/editar/{id}")
+	public String mostrarFormularioEdicion(@PathVariable("id") long id, Model model) {		
+		model.addAttribute("cliente", clienteServicio.findById(id));
+		return "misDatos";
+	}
+
 }
