29733d956fb47fe4d18087c73b56fe59acd6d785
Pedro <pedrofe.av@gmail.com>
2023-05-15 21:01:07 +0200

    buscar titulo comic

--- src/main/java/com/salesianos/triana/dam/principioProyFinal/repos/ComicRepositorio.java
+++ src/main/java/com/salesianos/triana/dam/principioProyFinal/repos/ComicRepositorio.java
@@ -1,9 +1,14 @@
 package com.salesianos.triana.dam.principioProyFinal.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.salesianos.triana.dam.principioProyFinal.model.Comic;
 
 public interface ComicRepositorio extends JpaRepository <Comic, Long> {
 
+	List<Comic> findByTituloContainsIgnoreCase(String titulo);
+	
+	
 }
--- src/main/java/com/salesianos/triana/dam/principioProyFinal/service/ComicServicio.java
+++ src/main/java/com/salesianos/triana/dam/principioProyFinal/service/ComicServicio.java
@@ -1,5 +1,7 @@
 package com.salesianos.triana.dam.principioProyFinal.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.salesianos.triana.dam.principioProyFinal.model.Comic;
@@ -7,5 +9,9 @@ import com.salesianos.triana.dam.principioProyFinal.repos.ComicRepositorio;
 
 @Service
 public class ComicServicio extends BaseServiceImpl<Comic, Long, ComicRepositorio>{
+	
+	public List<Comic> findByNombre(String titulo){
+		return this.repository.findByTituloContainsIgnoreCase(titulo);
+	}
 
 }
