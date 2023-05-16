aeee4dc0c3f7ebbea7a7a0b9cefb17ec6e422314
Pedro <pedrofe.av@gmail.com>
2023-05-16 21:18:53 +0200

    arreglado login y crud de Juegos de Mesa

--- src/main/java/com/salesianos/triana/dam/principioProyFinal/controler/JMcontroller.java
+++ src/main/java/com/salesianos/triana/dam/principioProyFinal/controler/JMcontroller.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,5 +40,32 @@ public class JMcontroller {
 	 public String procesarFormularioJM(@ModelAttribute("juegoMesa") JuegoMesa jm) {
 		 jmServicio.save(jm);
 		 return"redirect:/admin/listaJM";
+	 }
+	 
+	 @GetMapping("/editarJuegoMesa/{id}")
+	 public String monstrarFormularioJuegoMesa(@PathVariable("id") Long id, Model model) {
+		 JuegoMesa jmEditar = jmServicio.findById(id).orElse(null);
+		 if(jmEditar != null) {
+			 model.addAttribute("juegoMesa", jmEditar);
+			 model.addAttribute("editoriales", editorialServicio.findAll());
+			 return "altaJuegoMesa";
+		 }else {
+			 return "redirect:/admin/nuevoJuegoMesa";
+		 }
+	 }
+	 
+	 @PostMapping("/edutarJuegoMesa/submit")
+	 public String procesarFormularioJuegoMesa(@ModelAttribute("juegoMesa") JuegoMesa m) {
+		 jmServicio.edit(m);
+		 return "redirec:/admin/listaJM";
+	 }
+	 
+	 @GetMapping("/borrarJuegoMesa/{id}")
+	 public String borrarJuegoMesa(@PathVariable("id") Long id, Model model) {
+		 JuegoMesa jmEliminar = jmServicio.findById(id).orElse(null);
+		 if(jmEliminar != null) {
+			 jmServicio.delete(jmEliminar);
+		 }
+		 return "redirect:/admin/listaJM";
 	 }
 }
