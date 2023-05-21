9908ef59fdc323716639bc8614406214901d9110
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-05-21 21:44:31 +0200

    alta usuario contraseña

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,14 +1,18 @@
 let boton = document.getElementById('botonFormularioCliente');
 let contrasena = document.getElementById('contrasena');
 let contrasena2 = document.getElementById('contrasena2');
+let mensajeContraseña = document.getElementById('mensajeContraseña');
+
 boton.addEventListener('click', compararContrasenias);
 
-function compararContrasenias() {
+function compararContrasenias(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+
     if (contrasena.value === contrasena2.value) {
-        // Contraseñas coinciden
+        mensajeContraseña.style.display = 'none'; // Oculta el mensaje de error
+        boton.form.submit(); // Envía el formulario
     } else {
-        let mostrarmenaje = document.querySelector('.mensajeContraseña');
-        mostrarmenaje.style.display = 'block';
-        event.preventDefault(); // Evita el envío del formulario
+        mensajeContraseña.style.display = 'block'; // Muestra el mensaje de error
+        contrasena.focus(); // Coloca el foco en el campo de contraseña
     }
 }
