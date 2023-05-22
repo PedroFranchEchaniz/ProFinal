a0934b088d89bdc9ad04561e9604641e0ebefb6a
Pedro <pedrofe.av@gmail.com>
2023-05-22 20:48:49 +0200

    control de formulario

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,18 +1,34 @@
 let boton = document.getElementById('botonFormularioCliente');
 let contrasena = document.getElementById('contrasena');
 let contrasena2 = document.getElementById('contrasena2');
+let nombre = document.getElementById('nombre');
 let mensajeContraseña = document.getElementById('mensajeContraseña');
+let mensajeNombre = document.getElementById('mensajeNombre');
 
-boton.addEventListener('click', compararContrasenias);
+boton.addEventListener('click', validarCampos);
 
-function compararContrasenias(event) {
+function validarCampos(event) {
     event.preventDefault(); // Evita el envío del formulario por defecto
+    const nombreRegExp = /^[a-zA-Z\s]+$/; // Expresión regular para permitir solo letras y espacios
+    let validacionExitosa = true;
 
-    if (contrasena.value === contrasena2.value) {
-        mensajeContraseña.style.display = 'none'; // Oculta el mensaje de error
-        boton.form.submit(); // Envía el formulario
-    } else {
-        mensajeContraseña.style.display = 'block'; // Muestra el mensaje de error
+    if (contrasena.value !== contrasena2.value) {
+        mensajeContraseña.style.display = 'block'; // Muestra el mensaje de error de contraseña
         contrasena.focus(); // Coloca el foco en el campo de contraseña
+        validacionExitosa = false;
+    } else {
+        mensajeContraseña.style.display = 'none'; // Oculta el mensaje de error de contraseña
+    }
+
+    if (!nombreRegExp.test(nombre.value.trim())) {        
+        mensajeNombre.style.display = 'block'; // Muestra el mensaje de error de nombre
+        nombre.focus(); // Coloca el foco en el campo de nombre
+        validacionExitosa = false;
+    } else {
+        mensajeNombre.style.display = 'none'; // Oculta el mensaje de error de nombre
+    }
+
+    if (validacionExitosa) {
+        boton.form.submit(); // Envía el formulario si la validación fue exitosa
     }
 }
