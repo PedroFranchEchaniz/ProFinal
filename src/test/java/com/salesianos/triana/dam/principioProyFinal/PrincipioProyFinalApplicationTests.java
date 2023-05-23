4d426a91e6822bc67b24cb2d18f9d3c37e04596f
Pedro Franch <pedrofe.av@gmail.com>
2023-05-23 10:30:54 +0200

    cambios

--- src/test/java/com/salesianos/triana/dam/principioProyFinal/PrincipioProyFinalApplicationTests.java
+++ src/test/java/com/salesianos/triana/dam/principioProyFinal/PrincipioProyFinalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.salesianos.triana.dam.e01holamundo;
+package com.salesianos.triana.dam.principioProyFinal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
