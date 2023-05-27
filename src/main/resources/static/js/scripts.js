bedb8ed6f5305db9b3612cebcc93a5f580a13393
Pedro <pedrofe.av@gmail.com>
2023-05-27 19:54:43 +0200

    mejoras html

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,6 +1,4 @@
 let boton = document.getElementById('botonFormularioCliente');
-let contrasena = document.getElementById('contrasena');
-let contrasena2 = document.getElementById('contrasena2');
 let nombre = document.getElementById('nombre');
 let apellido = document.getElementById('apellido');
 let fechaNacimiento = document.getElementById('fechanacimiento');
@@ -9,102 +7,97 @@ let mensajeNombre = document.getElementById('mensajeNombre');
 let mensajeApellido = document.getElementById('mensajeApellido');
 let mensajeEdad = document.getElementById('mensajeEdad');
 
-
 let botonUsuario = document.getElementById('botonEditarUsuario');
-let urlImg = document.getElementById('imagenUsuario');
-let nombreUsuario= document.getElementById('nombreUsuario');
+let nombreUsuario = document.getElementById('nombreUsuario');
 let fechanacimientoUsuario = document.getElementById('fechanacimientoUsuario');
 let apellidoUsuario = document.getElementById('apellidoUsuario');
 let direccionUsuario = document.getElementById('direccionUsuario');
 let emailUsuario = document.getElementById('emailUsuario');
+let mensajeNombreUsuario = document.getElementById('mensajeNombreUsuario');
+let mensajeApellidoUsuario = document.getElementById('mensajeApellidoUsuario');
+let mensajeEdadUsuario = document.getElementById('mensajeEdadUsuario');
 
-
-botonUsuario.addEventListener('click', validarCamposUsuario);
 boton.addEventListener('click', validarCampos);
+botonUsuario.addEventListener('click', validarCamposUsuario);
 
 function validarCampos(event) {
-	event.preventDefault(); 
-	const nombreRegExp = /^[a-zA-Z\s]+$/; 
+	event.preventDefault();
+	const nombreRegExp = /^[a-zA-Z\s]+$/;
 	let validacionExitosa = true;
-
-
-	if (!nombreRegExp.test(nombreUsuario.value.trim())) {
-		mensajeNombre.textContent = 'Por favor, ingresa un nombre válido sin números ni caracteres especiales.';
-		mensajeNombre.style.display = 'block'; 
-		nombre.focus(); 
-		validacionExitosa = false;
-	} else {
-		mensajeNombre.style.display = 'none'; 
-	}
-
-	if (!nombreRegExp.test(apellido.value.trim())) {
-		mensajeApellido.textContent = 'Por favor, ingresa un apellido válido sin números ni caracteres especiales.';
-		mensajeApellido.style.display = 'block'; 
-		apellido.focus(); 
-		validacionExitosa = false;
-	} else {
-		mensajeApellido.style.display = 'none'; 
-	}
-	
-	const hoy = new Date();
-	const fechaCumple = new Date(fechaNacimiento.value);
-	const edad = hoy.getFullYear() - fechaCumple.getFullYear();
-
-	if (edad < 18) {
-		mensajeEdad.textContent = 'Debes ser mayor de edad para registrarte.';
-		mensajeEdad.style.display = 'block'; 
-		fechaNacimiento.focus(); 
-		validacionExitosa = false;
-	} else {
-		mensajeEdad.style.display = 'none'; 
-	}
-
-	if (validacionExitosa) {
-		boton.form.submit(); 
-	}
-}
-
-
-
-function validarCamposUsuario(event) {
-	event.preventDefault(); 
-	const nombreRegExp = /^[a-zA-Z\s]+$/; 
-	let validacionExitosa = true;
-
-	
 
 	if (!nombreRegExp.test(nombre.value.trim())) {
 		mensajeNombre.textContent = 'Por favor, ingresa un nombre válido sin números ni caracteres especiales.';
-		mensajeNombre.style.display = 'block'; 
-		nombre.focus(); 
+		mensajeNombre.style.display = 'block';
+		nombre.focus();
 		validacionExitosa = false;
 	} else {
-		mensajeNombre.style.display = 'none'; 
+		mensajeNombre.style.display = 'none';
 	}
 
 	if (!nombreRegExp.test(apellido.value.trim())) {
 		mensajeApellido.textContent = 'Por favor, ingresa un apellido válido sin números ni caracteres especiales.';
-		mensajeApellido.style.display = 'block'; 
-		apellido.focus(); 
+		mensajeApellido.style.display = 'block';
+		apellido.focus();
 		validacionExitosa = false;
 	} else {
-		mensajeApellido.style.display = 'none'; 
+		mensajeApellido.style.display = 'none';
 	}
-	
+
 	const hoy = new Date();
 	const fechaCumple = new Date(fechaNacimiento.value);
 	const edad = hoy.getFullYear() - fechaCumple.getFullYear();
 
 	if (edad < 18) {
 		mensajeEdad.textContent = 'Debes ser mayor de edad para registrarte.';
-		mensajeEdad.style.display = 'block'; 
-		fechaNacimiento.focus(); 
+		mensajeEdad.style.display = 'block';
+		fechaNacimiento.focus();
 		validacionExitosa = false;
 	} else {
-		mensajeEdad.style.display = 'none'; 
+		mensajeEdad.style.display = 'none';
 	}
 
 	if (validacionExitosa) {
-		boton.form.submit(); 
+		boton.form.submit();
+	}
+}
+
+function validarCamposUsuario(event) {
+	event.preventDefault();
+	const nombreRegExp = /^[a-zA-Z\s]+$/;
+	let validacionExitosa = true;
+
+	if (!nombreRegExp.test(nombreUsuario.value.trim())) {
+		mensajeNombreUsuario.textContent = 'Por favor, ingresa un nombre válido sin números ni caracteres especiales.';
+		mensajeNombreUsuario.style.display = 'block';
+		nombreUsuario.focus();
+		validacionExitosa = false;
+	} else {
+		mensajeNombreUsuario.style.display = 'none';
+	}
+
+	if (!nombreRegExp.test(apellidoUsuario.value.trim())) {
+		mensajeApellidoUsuario.textContent = 'Por favor, ingresa un apellido válido sin números ni caracteres especiales.';
+		mensajeApellidoUsuario.style.display = 'block';
+		apellidoUsuario.focus();
+		validacionExitosa = false;
+	} else {
+		mensajeApellidoUsuario.style.display = 'none';
+	}
+
+	const hoy = new Date();
+	const fechaCumple = new Date(fechanacimientoUsuario.value);
+	const edad = hoy.getFullYear() - fechaCumple.getFullYear();
+
+	if (edad < 18) {
+		mensajeEdadUsuario.textContent = 'Debes ser mayor de edad para registrarte.';
+		mensajeEdadUsuario.style.display = 'block';
+		fechanacimientoUsuario.focus();
+		validacionExitosa = false;
+	} else {
+		mensajeEdad.style.display = 'none';
+	}
+
+	if (validacionExitosa) {
+		botonUsuario.form.submit();
 	}
 }
